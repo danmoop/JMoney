d5a70d5a5a1dd70bcaab36a3f038c75dc0dcba49
dan322 <dandurnev0@gmail.com>
2018-04-06 19:51:33 +0300

    Sign for each currency, getters / setters

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,15 @@
+import misc.Debug;
+import money.Currency;
+
 public class Main
 {
     public static void main(String[] args)
     {
+        Currency myCurr = new Currency(500, "EUR");
 
+        Debug.Log("Amount: " + myCurr.getAmount());
+        Debug.Log("Currency: " + myCurr.getCurrencyType());
+        Debug.Log("Currency Str: " + myCurr.getAmountString());
+        Debug.Log("Text for display: " + myCurr.getText());
     }
 }
--- src/money/Currency.java
+++ src/money/Currency.java
@@ -3,21 +3,63 @@ package money;
 import misc.Debug;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Currency
 {
     private String[] currencyList = {"USD", "CAD", "AUD", "GBP", "RUB", "EUR"};
 
+    private Map<String, String> currencySigns;
+
+    private String currencyType;
+    private float amount;
+
     public Currency(float amount, String currencyType)
     {
         if(Arrays.asList(currencyList).contains(currencyType))
         {
-            
+            this.currencyType = currencyType;
+            this.amount = amount;
+
+            currencySigns = new HashMap<>();
+
+            currencySigns.put("USD", "$");
+            currencySigns.put("CAD", "$");
+            currencySigns.put("AUD", "$");
+            currencySigns.put("GBP", "£");
+            currencySigns.put("RUB", "\u20BD");
+            currencySigns.put("EUR", "€");
         }
 
         else
         {
             Debug.Error("Unknown currency: " + currencyType);
         }
+    }
+
+    public String getCurrencyType()
+    {
+        return currencyType;
+    }
+
+    public float getAmount()
+    {
+        return amount;
+    }
+
+    public String getAmountString()
+    {
+        return String.valueOf(amount);
+    }
+
+    public void setAmount(float amount)
+    {
+        this.amount = amount;
+    }
+
+    public String getText()
+    {
+        return currencySigns.get(currencyType) + amount;
     }
 }
